c331fbcde2066737bc4560b279765feb8e4fade4
MorglenRazor <r.gubaev@bk.ru>
2021-05-12 00:01:32 +0300

    Added variable layoutResId

--- app/src/main/java/me/morglenrazor/imdbcollector/ui/CurrentMovieFragment.kt
+++ app/src/main/java/me/morglenrazor/imdbcollector/ui/CurrentMovieFragment.kt
@@ -1,6 +1,16 @@
 package me.morglenrazor.imdbcollector.ui
 
+import android.os.Bundle
+import android.view.View
+import me.morglenrazor.imdbcollector.R
 import me.morglenrazor.imdbcollector.ui.base.BaseFragment
 
-class CurrentMovieFragment : BaseFragment() {
+class CurrentMovieFragment() : BaseFragment(){
+
+    override val layoutResId = R.layout.fragment_current_movie
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
